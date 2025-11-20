916a523a653c88fa482793568aac84aaa85c779e
gerardPolloRebozado <gerardit4@gmail.com>
2025-11-20 18:21:18 +0100

    fix game

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -5,13 +5,14 @@ import PlayContent from './client-page'
 import { Suspense } from 'react'
 import { GameSkeleton } from '@/components/game-components'
 
-export default async function PlayPage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined }}) {
+export default async function PlayPage({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }>}) {
   const cookieStore = await cookies()
   const userId = cookieStore.get('userId')?.value
   const playerName = cookieStore.get('playerName')?.value
+    const sp = await searchParams
 
   if (!userId || !playerName) {
-    const code = searchParams?.code || ''
+    const code = sp.code || ''
     const redirectUrl = `/register?redirect=${encodeURIComponent(`/play?code=${code}`)}`
     redirect(redirectUrl)
   }
